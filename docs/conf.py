e7ca0d63b24653339b74a41b445d027b47cb2c2a
Joseph Wayne Pardue <joepardue47@gmail.com>
2025-07-17 13:01:00 -0400

    Add Sphinx configuration file

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+project = 'wicdpico'
+copyright = '2025, Joe Pardue'
+author = 'Joe Pardue'
+release = '1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
