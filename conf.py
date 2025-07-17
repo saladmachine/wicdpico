68339a4a98e9a0bf04e2a06b931ad228310245d9
Joseph Wayne Pardue <joepardue47@gmail.com>
2025-07-17 13:26:36 -0400

    Exclude CircuitPython files from Sphinx build

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
 
 project = 'wicdpico'
 copyright = '2025, Joe Pardue'
@@ -8,19 +8,11 @@ author = 'Joe Pardue'
 release = '1.0'
 
 extensions = [
-    'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'code.py', 'boot.py']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
-autodoc_default_options = {
-    'members': True,
-    'undoc-members': True,
-    'show-inheritance': True,
-}
