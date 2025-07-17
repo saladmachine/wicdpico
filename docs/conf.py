4d98abd56ed834c37f89553f89f70b4d78281d28
Joseph Wayne Pardue <joepardue47@gmail.com>
2025-07-17 13:17:31 -0400

    Configure autodoc to find Python modules

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 project = 'wicdpico'
 copyright = '2025, Joe Pardue'
 author = 'Joe Pardue'
@@ -6,6 +10,7 @@ release = '1.0'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
 ]
 
 templates_path = ['_templates']
@@ -13,3 +18,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
